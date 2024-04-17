fee9324059fad3ae64899c4c35f4c7fbfd87daf6
aronmilenait <aronmilena.it@gmail.com>
2024-04-17 14:26:03 -0300

    Change grid in mobile

--- portfolio/src/pages/Skills.tsx
+++ portfolio/src/pages/Skills.tsx
@@ -29,7 +29,7 @@ const Skills = () => {
         <h4 className="text-4xl font-medium mb-6 text-pink-900 text-center">
           Skills
         </h4>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
           {skills.map((skill, index) => (
             <div
               key={index}
