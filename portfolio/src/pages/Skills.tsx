3d51394f4c4ac13848cfb3fed6fe22038ff1b8bb
aronmilenait <aronmilena.it@gmail.com>
2024-04-17 21:06:21 -0300

    Add effects to the skills section

--- portfolio/src/pages/Skills.tsx
+++ portfolio/src/pages/Skills.tsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+
 const Skills = () => {
   const skills = [
     "JavaScript",
@@ -23,24 +26,70 @@ const Skills = () => {
     "Bash",
   ];
 
+  const [isVisible, setIsVisible] = useState(false);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const skillsSection = document.getElementById("skills-section");
+      if (skillsSection) {
+        const sectionTop = skillsSection.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        setIsVisible(sectionTop < windowHeight);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isVisible) {
+      controls.start("visible");
+    }
+  }, [isVisible, controls]);
+
   return (
-    <section className="bg-gradient-to-r from-pink-50 to-pink-100 py-8">
+    <section
+      id="skills-section"
+      className="bg-gradient-to-r from-pink-50 to-pink-100 py-8"
+    >
       <div className="container mx-auto max-w-4xl px-4">
         <h4 className="text-4xl font-medium mb-6 text-pink-900 text-center">
           Skills
         </h4>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+        <motion.div
+          className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4"
+          variants={{
+            visible: {
+              transition: { staggerChildren: 0.1 },
+            },
+          }}
+          initial="hidden"
+          animate={controls}
+        >
           {skills.map((skill, index) => (
-            <div
+            <motion.div
               key={index}
-              className="bg-gradient-to-br from-pink-300 to-pink-500 rounded-lg shadow-lg hover:shadow-xl transform hover:-translate-y-1 transition duration-300 cursor-pointer"
+              className="bg-gradient-to-br from-pink-300 to-pink-500 rounded-lg shadow-lg overflow-hidden"
+              variants={{
+                hidden: { opacity: 0, scale: 0.8 },
+                visible: {
+                  opacity: 1,
+                  scale: 1,
+                  transition: { type: "spring", stiffness: 200 },
+                },
+              }}
             >
               <p className="text-lg text-white font-bold py-4 text-center">
                 {skill}
               </p>
-            </div>
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
