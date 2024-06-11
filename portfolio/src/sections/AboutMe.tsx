c892c76c6aa42974d5a31225601b10832c07910a
aronmilenait <aronmilena.it@gmail.com>
2024-06-11 09:39:18 -0300

    Update my personal information

--- portfolio/src/sections/AboutMe.tsx
+++ portfolio/src/sections/AboutMe.tsx
@@ -3,38 +3,49 @@ import { motion } from "framer-motion";
 const AboutMe = () => {
   return (
     <section
-      id="about"
-      className="relative py-16 bg-gradient-to-r from-pink-200 via-pink-50 to-pink-200 overflow-hidden"
+      id="about-me"
+      className="bg-gradient-to-r from-pink-50 to-pink-100 py-8"
     >
-      <div className="container mx-auto px-6 max-w-4xl">
+      <div className="container mx-auto max-w-4xl px-4">
         <motion.h2
-          className="text-4xl font-medium text-pink-900 mb-6 text-center relative z-10"
+          className="text-4xl font-medium text-pink-900 mb-8 text-center"
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
         >
           About Me
         </motion.h2>
-        <div className="relative z-10 text-lg text-pink-900 leading-relaxed space-y-6">
-          <motion.p
-            initial={{ opacity: 0, x: -20 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5, delay: 0.4 }}
-          >
-            Hi! I'm Milena Sol Aron, a Full Stack and Web Developer based in
-            Buenos Aires, Argentina. Currently, I work remotely at Bigger as a
-            Software Developer, and until May 2024, I also worked as a Marketing
-            Tech.
-          </motion.p>
-          <motion.p
-            initial={{ opacity: 0, x: -20 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5, delay: 0.8 }}
-          >
-            Additionally, I'm pursuing an associate's degree in programming at
-            the Universidad Tecnológica Nacional (UTN) since May 2024.
-          </motion.p>
-        </div>
+        <motion.p
+          className="text-lg text-pink-900 mb-4"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+        >
+          Hi! I'm Milena, a <b>Full Stack & Web Developer</b> based in Buenos
+          Aires, Argentina, with over a year of experience in the field.
+        </motion.p>
+        <motion.p
+          className="text-lg text-pink-900 mb-4"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+        >
+          Besides coding, I have almost five years of experience in{" "}
+          <b>Digital Marketing & Tech</b> and nine years of experience in{" "}
+          <b>Linux</b>.
+        </motion.p>
+        <motion.p
+          className="text-lg text-pink-900 mb-4"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+        >
+          Additionally, I have been pursuing an{" "}
+          <b>associate degree in programming</b> at the Universidad Tecnológica
+          Nacional (UTN) since May 2024.
+        </motion.p>
+
+        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3"></div>
       </div>
     </section>
   );
