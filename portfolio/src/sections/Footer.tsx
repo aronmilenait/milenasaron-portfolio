c719ad7305cc75aefe3f68f38882240544817b39
aronmilenait <aronmilena.it@gmail.com>
2024-06-19 14:53:46 -0300

    Correct typo in footer

--- portfolio/src/sections/Footer.tsx
+++ portfolio/src/sections/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-gradient-to-b from-pink-500 to-pink-400 text-pink-50 p-8 font-medium shadow-md">
       <div className="container mx-auto text-center text-md">
         <p>
-          Made with by Milena S. Aron using Vite, TypeScript, React and Tailwind
+          Made by Milena S. Aron using Vite, TypeScript, React and Tailwind
           CSS.
         </p>
       </div>
